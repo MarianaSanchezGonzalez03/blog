6a8d908ad0c19c32f72c2643e2185e44a397f386
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2021-07-07 20:13:36 -0500

    Creando la barra busqueda

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 
                 <footer>
-                <p class="copyright">Copyright @ 2015-2016 Mariana </p>
+                <p class="copyright">Copyright @ 2020-2021 Mariana </p>
                 </footer>
             </body>
             </html>
